6d37da74baa168c8f0e8c8f25746213e3d7a5bee
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-26 21:41:53 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,8 +4,7 @@ def my_collect(students)
   i = 0 
   while i < array.length 
     yield(array[i])
-      firstnames = array[i].split(" ").first
-      newarray.push(firstnames)
+      
     i += 1
   end 
   newarray
