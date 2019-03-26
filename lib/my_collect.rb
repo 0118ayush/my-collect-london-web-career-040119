7ea5054aa04d7638b2e4db6b362358c47370fcfd
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-26 21:27:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,13 @@
 def my_collect(array)
+  
+  capitalized = []
   i = 0 
-  
   while i < array.length 
-    
+    yield(array[i])
+      
+    i += 1
   end 
-  
+  capitalized
 end 
 
+my_collect(array){|x| capitalized.push(x.capitalize)}
