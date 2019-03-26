4fe2068647a365ea56c4f5198f441abb1b63cdfc
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-26 21:48:58 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,10 @@
-def my_collect(students)
+def my_collect(array)
   
   newarray = []
   i = 0 
   while i < array.length 
     yield(array[i])
-      newarray.push(array[i].upcase)
+    
     i += 1
   end 
   newarray
