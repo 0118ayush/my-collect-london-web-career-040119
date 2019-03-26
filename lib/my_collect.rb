f9b5faf1944d431279f78e772a9e172ad15111a3
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-26 21:52:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,11 +3,10 @@ def my_collect(array)
   newarray = []
   i = 0 
   while i < array.length 
-    temp = yield(array[i])
-    newarray.push(temp)
+    yield(array[i])
+    newarray.push(array[i].upcase)
     i += 1
   end 
   newarray
 end 
 
-my_collect(array){|x| x.upcase}
